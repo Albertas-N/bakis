40ec23ddc4a1bee654c47c1a11670a5de0ba0a34
albertnaimovicisvf19 <a.naimovic@stud.vilniustech.lt>
2022-12-29 12:55:18 +0200

    feat: added simple sqlite db

--- server/server/settings.py
+++ server/server/settings.py
@@ -73,11 +73,33 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+#cia prisijungimas prie postgresql, nepadarytas pilnai dar
+
+DATABASES={
+
+  'default':{
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'Filtravimo-Sistemos',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'password',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+  }
+
 }
 
 
