2fd0bca8ed90fbb85cadda85b5375dcfedf9d2e6
sarunassarunassarunassarunas <sarunas.vitkus@stud.vilniustech.lt>
2023-01-11 21:49:44 +0200

    Added proper styling

--- server/webv1/views.py
+++ server/webv1/views.py
@@ -14,22 +14,6 @@ def categories(request):
     return render(request, 'web/categories.html', {'category': category})
 
 
-def restaurants(request):
-    return render(request, 'web/restaurants.html')
-
-
-def clubs(request):
-    return render(request, 'web/clubs.html')
-
-
-def bars(request):
-    return render(request, 'web/bars.html')
-
-
-def attractions(request):
-    return render(request, 'web/attractions.html')
-
-
 def about_view(request):
     return render(request, 'web/about.html')
 
@@ -37,7 +21,6 @@ def about_view(request):
 def my_view(request):
     # Connect to the database
     conn = sqlite3.connect('sqlite_db.db')
-
 
 # creating a view to retrieve all category types and categories from database
 def home(request):
@@ -56,4 +39,4 @@ def component_view(request):
         return render(request, 'categories.html', context)
     else:
         messages.error(request, "Invalid request method.")
-        return redirect('home')
+        return redirect('home.html')
